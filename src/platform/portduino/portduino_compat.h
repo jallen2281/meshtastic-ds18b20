b639668247899a5847ccf022b9e41b3650c6885c
Jared Allen <jaallen@liquidweb.com>
2026-06-13 11:24:02 -0400

    fix(portduino): always provide interrupt stubs in forced include header

--- src/platform/portduino/portduino_compat.h
+++ src/platform/portduino/portduino_compat.h
@@ -2,7 +2,11 @@
 // Force-included into every compilation unit for the rpi3bplus_rfm95_oled Portduino build.
 // Provides Arduino interrupt wrappers that third-party libs (e.g. OneWire) expect
 // but that Portduino/Linux does not supply.
-#if defined(ARCH_PORTDUINO)
+
+#ifndef noInterrupts
 static inline void noInterrupts() {}
+#endif
+
+#ifndef interrupts
 static inline void interrupts() {}
 #endif
